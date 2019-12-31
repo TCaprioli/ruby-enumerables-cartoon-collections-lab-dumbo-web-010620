95f39c5b4b24840410a660f5104af4d131eefdd2
Tyler Caprioli <capriolityler@gmail.com>
2019-12-31 23:38:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(call)
  call.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(maybe_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  maybe_cheese.include
 end
